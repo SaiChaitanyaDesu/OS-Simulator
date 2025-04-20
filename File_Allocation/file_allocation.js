bdfdf47852fb24e620f46b8daf3fc24b10856079
SaiChaitanyaDesu <167234980+SaiChaitanyaDesu@users.noreply.github.com>
2025-04-20 22:52:17 +0530

    Update file_allocation.js

--- File_Allocation/file_allocation.js
+++ File_Allocation/file_allocation.js
@@ -402,3 +402,12 @@ function reset() {
 document.addEventListener('DOMContentLoaded', function() {
   renderDisk();
 });
+
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 10) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
